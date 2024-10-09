d64979e7fffc284f1237e851d0ad9ba801db1bed
chuiiboi <brendancroft2003@gmail.com>
2024-10-09 19:15:21 +1300

    Fix article.entity.ts

--- backend/src/articles/article.entity.ts
+++ backend/src/articles/article.entity.ts
@@ -3,17 +3,26 @@ import { Document } from 'mongoose';
 
 @Schema()
 export class Article extends Document {
-  @Prop({ required: true })
+  @Prop({ required: true })  // 标题是必需字段
   title: string;
 
-  @Prop({ required: true })
-  content: string;
-
-  @Prop({ required: true })
+  @Prop()  // 作者可以是可选字段
   author: string;
 
-  @Prop({ required: true })
-  publishDate: Date;
+  @Prop()  // 文章发布日期
+  publicationDate: string;
+
+  @Prop()  // DOI 文章的数字对象唯一标识符
+  DOI: string;
+
+  @Prop()  // 文章摘要
+  abstract: string;
+
+  @Prop()  // 上传日期
+  uploadDate: string;
+
+  @Prop()  // 链接到文章的 URL
+  link: string;
 }
 
 export const ArticleSchema = SchemaFactory.createForClass(Article);
